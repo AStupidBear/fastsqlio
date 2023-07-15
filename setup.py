b6dd314466a8ee3ab91d9c80bf5cbcb323a2976c
Yao Lu <luyaocns@gmail.com>
2023-07-15 12:54:41 +0800

    fix setup

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     packages=find_packages(),
     install_requires=["pandas", "sqlalchemy==1.4.48", "pymysql", "sql_metadata", "connectorx; python_version > '3.6'",
                       "pandahouse@git+https://github.com/AStupidBear/pandahouse.git@parquet",
-                      "clickhouse-driver@git+https://github.com/AStupidBear/clickhouse-driver.git@extensionarray",
-                      "clickhouse-sqlalchemy[lz4]@git+https://github.com/AStupidBear/clickhouse-sqlalchemy.git@dtprec"]
+                      "clickhouse-driver[lz4]@git+https://github.com/AStupidBear/clickhouse-driver.git@extensionarray",
+                      "clickhouse-sqlalchemy@git+https://github.com/AStupidBear/clickhouse-sqlalchemy.git@dtprec"]
 )
