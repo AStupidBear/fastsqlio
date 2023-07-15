e2de2f829d079768f764bea5ac5243acfaa0aa2f
Yao Lu <luyaocns@gmail.com>
2023-07-15 12:50:32 +0800

    requires clickhouse-driver and clickhouse-sqlalchemy

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(
     description="Fast read_sql and to_sql",
     packages=find_packages(),
     install_requires=["pandas", "sqlalchemy==1.4.48", "pymysql", "sql_metadata", "connectorx; python_version > '3.6'",
-                      "pandahouse@git+https://github.com/AStupidBear/pandahouse.git@parquet"],
+                      "pandahouse@git+https://github.com/AStupidBear/pandahouse.git@parquet",
+                      "clickhouse-driver@git+https://github.com/AStupidBear/clickhouse-driver.git@extensionarray",
+                      "clickhouse-sqlalchemy[lz4]@git+https://github.com/AStupidBear/clickhouse-sqlalchemy.git@dtprec"]
 )
