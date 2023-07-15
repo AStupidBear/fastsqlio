10d8fc6b1317aa0526f6b71c6ba2b6c03a3ddd11
Yao Lu <luyaocns@gmail.com>
2023-07-15 10:48:28 +0800

    sqlalchemy==1.4.48

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="0.1.0",
     description="Fast read_sql and to_sql",
     packages=find_packages(),
-    install_requires=["pandas", "sqlalchemy", "pymysql", "sql_metadata", "connectorx; python_version > '3.6'",
+    install_requires=["pandas", "sqlalchemy==1.4.48", "pymysql", "sql_metadata", "connectorx; python_version > '3.6'",
                       "pandahouse@git+https://github.com/AStupidBear/pandahouse.git@parquet"],
 )
